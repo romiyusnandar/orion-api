f20ac2e10a6432bd619ac1de663aaca27ce20ab0
romiyusnandar <yusromi04@gmail.com>
2024-07-08 21:35:19 +0700

    Add changelog source

---
+++ routes/changelog.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const changelog = [
+  {
+    _id: 101,
+    date: "July 08 2024",
+    changes: [
+      "Codename COSMIC",
+      "Merge QPR3",
+      "Merge july security patch"
+    ]
+  }
+]
+
+// get all data
+router.get("/changelog", (req, res) => {
+  res.send(changelog)
+});
